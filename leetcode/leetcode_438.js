700c0a7bc056062b3a2a2013553cd9885eaf7059
zzzzsub <ji12subin@gmail.com>
2022-01-11 04:58:16 +0900

    leetcode_438 solution #2

--- leetcode/leetcode_438.js
+++ leetcode/leetcode_438.js
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/find-all-anagrams-in-a-string/
 //438. Find All Anagrams in a String
 
+//Solution #1
 var findAnagrams = function(s, p) {
   let res = [];
   //edge case
@@ -38,3 +39,43 @@ var findAnagrams = function(s, p) {
   }
   return res;
 };
+
+//Solution #2
+function stringAnagrams(string, pattern){
+    let res=[];
+    //edge case
+    if(pattern.length > string.length) return res;
+  
+    const patternMap = new Map();
+    for(let char of pattern){
+      if(patternMap.has(char)) patternMap.set(char, patternMap.get(char)+1);
+      else patternMap.set(char, 1);
+    }
+    
+    let left=0, right=0;
+    let counter=pattern.length;
+  
+    while(right<string.length){
+      if(patternMap.has(string[right])){
+        patternMap.set(string[right], patternMap.get(string[right])-1);
+      }
+      counter--;
+      if(counter===0){
+        let mCount = 0;
+        for(let [key, value] of patternMap){
+          mCount++;
+          if(value !== 0) break;
+          else if(mCount === patternMap.size) res.push(left);
+        }
+        //console.log(patternMap)
+        //console.log(res)
+        if(patternMap.has(string[left])){
+          patternMap.set(string[left], patternMap.get(string[left])+1);
+        }
+        counter++;
+        left++;
+      }
+      right++;
+    }
+    return res;
+  }
