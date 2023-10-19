9086a5f1b3c9cb0c5431484e15a140696cb79091
zzzzsb <ji12subin@gmail.com>
2023-10-19 15:47:21 +0900

    [23/10/19] 백준 브론즈 2 / 블랙잭

--- 백준/python/boj_2798.py
+++ 백준/python/boj_2798.py
@@ -6,27 +6,23 @@ N, M = map(int, input().split(" "))
 card = list(map(int, input().split()))
 
 # 백트래킹 풀이
-check = [False] * N
 answer = 0
 cardArr = []
 
-def dfs(depth):
+def dfs(idx, cnt):
     global answer
-    if depth == 3:
+    if cnt == 3:
         total = sum(cardArr)
         if total > answer and total <= M:
             answer = total
         return
 
-    for i in range(N):
-        if not check[i]:
+    for i in range(idx, N):
             cardArr.append(card[i])
-            check[i] = True
-            dfs(depth+1)
+            dfs(i+1, cnt+1)
             cardArr.pop()
-            check[i] = False
 
-dfs(0)
+dfs(0,0)
 print(answer)
 
 # 조합 풀이
