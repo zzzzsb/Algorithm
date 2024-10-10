c4b0b107f11765c1f85e926c4f4ab6b21e3c70e7
zzzzsb <ji12subin@gmail.com>
2024-10-10 19:29:11 +0900

    [24/10/09] 프로그래머스 / Level 3 / 네트워크

---
+++ 프로그래머스/python/Level 3/네트워크.py
@@ -0,0 +1,66 @@
+# 재귀함수로 dfs 구현
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    def dfs(curIdx):
+        visited[curIdx] = True
+        for i in range(n):
+            if not visited[i] and computers[curIdx][i] == 1:
+                dfs(i)
+        
+        
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    return answer
+
+# 스택으로 dfs 구현
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    def dfs(start):
+        stack = deque()
+        stack.append(start)
+        
+        while stack:
+            curNode = stack.pop()
+            visited[curNode] = True
+            for i in range(n):
+                if not visited[i] and computers[curNode][i] == 1:
+                    stack.append(i)
+        
+        
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    return answer
+
+# bfs 사용한 풀이
+from collections import deque
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    def bfs(curIdx):
+        queue = deque()
+        queue.append(curIdx)
+        while queue:
+            curIdx = queue.popleft()
+            visited[curIdx] = True
+            for i in range(n):
+                if not visited[i] and computers[curIdx][i] == 1:
+                    queue.append(i)
+        
+        
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            answer += 1
+            
+    return answer
